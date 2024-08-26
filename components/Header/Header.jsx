a067848562a6e707d8cc67ba6c3aadb32642cda3
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-26 16:21:27 +1000

    init header with just elements positioning

---
+++ components/Header/Header.jsx
@@ -0,0 +1,68 @@
+import Image from "next/image"
+import Link from "next/link"
+import logoPuglia from '@/public/logoPuglia.svg'
+import styles from '@/styles/header.module.css'
+import facebookIcon from '@/public/facebookIcon.svg'
+import InstagramIcon from '@/public/instagramIcon.svg'
+import { Autour_One } from "next/font/google"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <nav>
+                <Link href='/'>Logo Sito</Link>
+                <Link href='/'>Home</Link>
+                <Link href='/posts'>Blog</Link>
+                <Link href='/guides'>Guides</Link>
+                <Link href='/Events'>Events</Link>
+                <Link href='/about'>About</Link>
+            </nav>
+            <div className={styles.pugliaLogo}>
+                <Image
+                    src={logoPuglia}
+                    alt="logo puglia region"
+                    height={100}
+                    width={100}
+                    style={{
+                        width: '60px',
+                        height: 'auto'
+                    }}
+
+                />
+            </div>
+            <div className={styles.socialBar}>
+                <div className={styles.socialSection}>
+                    <Link href={'facebook.com'}>
+                        <Image
+                            src={facebookIcon}
+                            alt='facebook icon'
+                            height={100}
+                            width={100}
+                            style={{
+                                width: '30px',
+                                height: 'auto'
+                            }}
+                        />
+                    </Link>
+                    <Link href={'instagram.com'}>
+                        <Image
+                            src={InstagramIcon}
+                            alt='instagram icon'
+                            height={100}
+                            width={100}
+                            style={{
+                                width: '30px',
+                                height: 'auto'
+                            }}
+                        />
+                    </Link>
+                </div>
+                <div className={styles.authSection}>
+                    <Link href='/login' className={styles.logInLink}>Login</Link>
+                    <Link href='/register' className={styles.registerLink}>Register</Link>
+                </div>
+            </div>
+
+        </header>
+    )
+}
