674520376efb5f806de0406cf0aa902f4624057f
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-27 23:34:02 +1000

    adds config to the authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,13 +6,36 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
+            id:'credentials',
             name: 'Credentials',
             credentials: {
-                name: { label: 'Name', type: 'text', placeholder: 'John' },
-                lastName: { label: 'Last Name', type: 'text', placeholder: 'Smith' },
+                email: { label: 'Email', type: 'email' },
                 password: { label: 'Password', type: 'password' }
-
-            }
+            },
+            async authorize(credentials, req) {
+                // // You need to provide your own logic here that takes the credentials
+                // // submitted and returns either a object representing a user or value
+                // // that is false/null if the credentials are invalid.
+                // // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+                // // You can also use the `req` object to obtain additional parameters
+                // // (i.e., the request IP address)
+                // const res = await fetch("/your/endpoint", {
+                //   method: 'POST',
+                //   body: JSON.stringify(credentials),
+                //   headers: { "Content-Type": "application/json" }
+                // })
+                // const user = await res.json()
+          
+                // // If no error and we have user data, return it
+                // if (res.ok && user) {
+                //   return user
+                // }
+                // // Return null if user data could not be retrieved
+                // return null
+                console.log('CREDENTIALS',credentials)
+                console.log('REQ',req)
+                return true
+              }
         })
         ,
         GoogleProvider({
@@ -29,16 +52,34 @@ const handler = NextAuth({
     ],
     callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
+            console.log("USER:", user)
+            console.log('PROFILE', profile)
+            console.log('EMAIL:', email)
+            console.log('CREDENTIALS', credentials)
+            if (account.provider == 'google') {
+                return true
+            }else{
+                console.log('ciao')
+                return false
+            }
+
+            //user.email
+            //user.image
             //profile.given_name
             //profile.family_name
-            //profile.email
-            console.log(credentials)
+            // console.log(account)
             // return true
+        },
+        async session({ session, token, user }) {
+            console.log('SESSION', session)
+            console.log('TOKEN', token)
+            console.log('USER', user)
+            return session
         }
+    },
+    pages: {
+        signIn: '/login'
     }
-    // pages:{
-    //     signIn:'/login'
-    // }
 
 })
 
