372dbea0b0a8afcac933e708bb5af170e67e0be6
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-29 14:08:01 +1000

    config auth options,providers and callbacks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,16 @@
-import NextAuth from "next-auth"
-import connectDB from "@/config/mongoDB/connectDB"
+import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
-import CredentialsProvider from 'next-auth/providers/credentials'
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectDB from "@/config/mongoDB/connectDB";
+import User from "@/models/User";
+import bcrypt from 'bcryptjs'
 
-const handler = NextAuth({
+
+export const authOptions = {
     providers: [
-        CredentialsProvider({
-            id:'credentials',
-            name: 'Credentials',
-            credentials: {
-                email: { label: 'Email', type: 'email' },
-                password: { label: 'Password', type: 'password' }
-            },
-            async authorize(credentials, req) {
-                // // You need to provide your own logic here that takes the credentials
-                // // submitted and returns either a object representing a user or value
-                // // that is false/null if the credentials are invalid.
-                // // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-                // // You can also use the `req` object to obtain additional parameters
-                // // (i.e., the request IP address)
-                // const res = await fetch("/your/endpoint", {
-                //   method: 'POST',
-                //   body: JSON.stringify(credentials),
-                //   headers: { "Content-Type": "application/json" }
-                // })
-                // const user = await res.json()
-          
-                // // If no error and we have user data, return it
-                // if (res.ok && user) {
-                //   return user
-                // }
-                // // Return null if user data could not be retrieved
-                // return null
-                console.log('CREDENTIALS',credentials)
-                console.log('REQ',req)
-                return true
-              }
-        })
-        ,
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENTID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
             authorization: {
                 params: {
                     prompt: "consent",
@@ -48,41 +18,58 @@ const handler = NextAuth({
                     response_type: "code"
                 }
             }
+
+        }),
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                username: { label: "email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                await connectDB()
+
+                const user = await User.findOne({ email: credentials.email })
+                if (user) {
+                    const matchingPassword = await bcrypt.compare(credentials.password, user.password)
+                    if (matchingPassword) {
+                        return user
+                    }
+                    return null
+
+                }
+                return null
+
+            }
         })
     ],
     callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            console.log("USER:", user)
-            console.log('PROFILE', profile)
-            console.log('EMAIL:', email)
-            console.log('CREDENTIALS', credentials)
-            if (account.provider == 'google') {
-                return true
-            }else{
-                console.log('ciao')
-                return false
+        async jwt({ token, user }) {
+            //from the user object returned from authorize, store info in token
+            if (user) {
+                token.userId = user._id
+                token.userPicture = user.picture
+                token.isAdmin = user.isAdmin
             }
-
-            //user.email
-            //user.image
-            //profile.given_name
-            //profile.family_name
-            // console.log(account)
-            // return true
+            return token
         },
-        async session({ session, token, user }) {
-            console.log('SESSION', session)
-            console.log('TOKEN', token)
-            console.log('USER', user)
+        async session({ session, token }) {
+            //get the info about the user from the token 
+            if (token) {
+                session.user.userId = token.userId
+                session.user.image = token.userPicture
+                session.user.isAdmin = token.isAdmin
+            }
             return session
         }
+
     },
     pages: {
         signIn: '/login'
     }
 
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
-
-
