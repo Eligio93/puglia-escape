dcac123c0a753e38e92d61000dfb3e66eeffda13
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-29 14:06:47 +1000

    pass session(got from server)  to the provider

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import SessionProvider from '@/config/Authentication/SessionProvider'
-import { getServerSession } from "next-auth";
 import Header from "@/components/Header/Header";
 import './global.css'
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,16 +14,18 @@ const inter = Inter({ subsets: ["latin"] });
 //   keywords:'blog puglia, discover puglia, plan an holiday to puglia, holiday puglia'
 // };
 
-export default function RootLayout({ children }) {
-  const session = getServerSession();
+export default async function RootLayout({ children }) {
+  const session = await getServerSession(authOptions)
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <SessionProvider session={session}>
+    <SessionProvider session={session}>
+      <html lang="en">
+        <body className={inter.className}>
+
           <Header />
           {children}
-        </SessionProvider>
-      </body>
-    </html>
+
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
