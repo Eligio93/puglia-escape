db1ee870acfe59ca18f8e6d46573cf6c2408652e
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-29 14:05:17 +1000

    adds remote pattern for pixabay

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
+    images: {
+        remotePatterns: [
             {
-                protocol:'https',
-                hostname:'images.ctfassets.net',
-                port:'',
-                pathname:'/**'
-            },{
-                protocol:'https',
-                hostname:'lh3.googleusercontent.com',
-                port:'',
-                pathname:'/**'
+                protocol: 'https',
+                hostname: 'images.ctfassets.net',
+                port: '',
+                pathname: '/**'
+            }, {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**'
+            }, {
+                protocol: 'https',
+                hostname: 'cdn.pixabay.com',
+                port: '',
+                pathname: '/**'
             }
         ]
     }
