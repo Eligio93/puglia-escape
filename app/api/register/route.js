d1d7e01d55047093e1e1264fa5d2df48afc0839b
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-29 14:09:50 +1000

    init post route to create a new User

---
+++ app/api/register/route.js
@@ -0,0 +1,35 @@
+import User from "@/models/User";
+import connectDB from "@/config/mongoDB/connectDB";
+import bcrypt from 'bcryptjs'
+import { NextResponse } from "next/server";
+import googleIcon from '@/public/googleIcon.svg'
+
+
+export const POST = async (req) => {
+    const body = await req.json();
+    await connectDB();
+    const existingUser = await User.findOne({ email: body.email })
+    if (existingUser) {
+        return NextResponse.json('This email has been already registered', { status: 409 })
+    }
+    const hashedPassword = await bcrypt.hash(body.password, 8);
+    try {
+        const user = new User({
+            name: body.name,
+            lastName: body.lastName,
+            email: body.email,
+            password: hashedPassword,
+            picture:null,
+            isAdmin: false
+        })
+        await user.save();
+        return NextResponse.json('User Created Correctly',{status:200})
+
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json('Server Error - Impossible to Register', { status: 500 })
+
+    }
+}
+
+
