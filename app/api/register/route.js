6fae79978c7f68611efb007f1af24efad7f27f63
Eligio93 <eligio.cristantielli@gmail.com>
2024-09-21 12:42:33 +1000

    use default image when user get created

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -19,7 +19,7 @@ export const POST = async (req) => {
             lastName: body.lastName,
             email: body.email,
             password: hashedPassword,
-            picture:null,
+            picture:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
             isAdmin: false
         })
         await user.save();
