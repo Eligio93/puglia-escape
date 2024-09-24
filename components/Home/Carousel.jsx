ab62f4844515eeb69dfd25e8a6cd06ea5c98a632
Eligio93 <eligio.cristantielli@gmail.com>
2024-09-24 19:28:05 +1000

    change position of nav buttons

--- components/Home/Carousel.jsx
+++ components/Home/Carousel.jsx
@@ -4,12 +4,13 @@ import { useCallback } from 'react'
 import styles from '@/styles/home.module.css'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
+import ClassNames from 'embla-carousel-class-names'
 import Image from 'next/image'
 
 
 
 export default function EmblaCarousel({ posts }) {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ stopOnMouseEnter: true, stopOnInteraction: false })])
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ stopOnMouseEnter: true, stopOnInteraction: false }),ClassNames({inView:''})])
 
     const scrollPrev = useCallback(() => {
         if (emblaApi) emblaApi.scrollPrev()
@@ -22,11 +23,10 @@ export default function EmblaCarousel({ posts }) {
     console.log(posts)
     return (
         <div className={styles.embla} >
-            <button className={styles.embla__prev} onClick={scrollPrev}>Prev</button>
+
             <div className={styles.embla__viewport} ref={emblaRef}>
                 <div className={styles.embla__container}>
                     {posts.map((post) => <div key={post.sys.id} className={styles.embla__slide}>
-
                         <Image
                             className={styles.carouselImg}
                             src={'https:' + post.fields.mainImage.fields.file.url}
@@ -34,13 +34,15 @@ export default function EmblaCarousel({ posts }) {
                             height={post.fields.mainImage.fields.file.details.image.height}
                             width={post.fields.mainImage.fields.file.details.image.width}
                         />
-
                         {post.fields.postTitle}
                     </div>)}
                 </div>
             </div>
+            <div className={styles.carouselNav}>
+                <button className={styles.embla__prev} onClick={scrollPrev}>Prev</button>
+                <button className={styles.embla__next} onClickCapture={scrollNext}>Next</button>
+            </div>
 
-            <button className={styles.embla__next} onClickCapture={scrollNext}>Next</button>
         </div>
     )
 }
