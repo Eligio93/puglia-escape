5a799bd68353a7d15e6c034404553a8b8203ed30
Eligio93 <eligio.cristantielli@gmail.com>
2024-09-28 13:16:27 +1000

    import style module

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,4 +1,6 @@
 import { getPosts } from "@/config/contentful/client";
+import BlogPosts from "@/components/Blog/BlogPosts";
+import styles from '@/styles/blog.module.css'
 
 
 
@@ -6,13 +8,14 @@ export default async function Blog({ searchParams }) {
     const cityQuery = searchParams.city ? 'city' + searchParams.city.split(' ').join('') : null;
     const categoryQuery = searchParams.category ? 'category' + searchParams.category.split(' ').join('') : null;
     const posts = await getPosts(categoryQuery, cityQuery);
-    console.log(posts)
+
 
     //controllare i parametri e fare il get post con i parametri selezionati altrimenti gettutti i post
     return (
-        <div className="blog">
-            <h1>Blog</h1>
-
+        <div className={styles.blog}>
+            <BlogPosts
+                posts={posts.items}
+            />
         </div>
 
 
