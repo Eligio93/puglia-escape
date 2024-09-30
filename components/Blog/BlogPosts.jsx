8baaf1d77c7e2ecc6de629baac95afe3ddb29785
Eligio93 <eligio.cristantielli@gmail.com>
2024-09-30 19:27:07 +1000

    adds author section to the listed blogPost

--- components/Blog/BlogPosts.jsx
+++ components/Blog/BlogPosts.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import styles from '@/styles/blog.module.css'
 
 export default async function BlogPosts({ posts }) {
+    // console.log(posts[0].fields.postAuthor.fields.authorPicture)
     return (
         <div className={styles.blogPosts} >
             {posts.length > 0 && posts.map((post) => <Link href={'/posts/' + post.fields.postSlug} key={post.sys.id} className={styles.blogPost}>
@@ -17,6 +18,16 @@ export default async function BlogPosts({ posts }) {
                 <p className={styles.blogPostDate}>{post.fields.publishingDate}</p> {/*date needs to be formatted with date-fns*/}
                 <p className={styles.blogPostTitle}>{post.fields.postTitle}</p>
                 <p className={styles.blogPostDescription}>{post.fields.postDescription}</p>
+                <div className={styles.authorInfo}>
+                    <Image 
+                        src={'https:' + post.fields.postAuthor.fields.authorPicture.fields.file.url}
+                        alt = {post.fields.postAuthor.fields.authorPicture.fields.description}
+                        height={236}
+                        width={236}
+                    />
+                    <p>{post.fields.postAuthor.fields.authorName}</p>
+
+                </div>
             </Link>)}
             {posts.length == 0 && <p>No posts found</p>}
         </div>
