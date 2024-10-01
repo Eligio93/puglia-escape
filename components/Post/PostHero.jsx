a9ec9ea830eb9cee1bf23e4106565517ba41c6fe
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-01 21:47:51 +1000

    init component

---
+++ components/Post/PostHero.jsx
@@ -0,0 +1,22 @@
+import style from '@/styles/post.module.css'
+import Image from 'next/image'
+
+export default function PostHero({ post }) {
+    return (
+        <>
+        <section className={style.postHero}>
+            <h1 className={style.postTitle}>{post.items[0].fields.postTitle}</h1>
+            <div className={style.postMainImageBox}>
+                <Image
+                    className={style.postMainImage}
+                    src={'https:' + post.items[0].fields.mainImage.fields.file.url}
+                    alt={post.items[0].fields.mainImage.fields.description}
+                    fill={true}
+                />
+            </div>
+            <h2 className={style.postSubtitle}>{post.items[0].fields.postSubtitle}</h2>
+        </section>
+        <hr />
+        </>
+    )
+}
