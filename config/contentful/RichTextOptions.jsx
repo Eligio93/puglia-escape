4582380c5c91682b68de20051a7e1dfbac993253
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-02 23:41:53 +1000

    adds option for when adding an image to the body

--- config/contentful/RichTextOptions.jsx
+++ config/contentful/RichTextOptions.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 export const options = {
-    renderNode:{
-        [INLINES.ENTRY_HYPERLINK]:(node,children) => <Link href={'/posts/'+node.data.target.fields.postSlug}>{node.content[0].value}</Link>
+    renderNode: {
+        [INLINES.ENTRY_HYPERLINK]: (node, children) => <Link href={'/posts/' + node.data.target.fields.postSlug}>{node.content[0].value}</Link>,
+        [BLOCKS.EMBEDDED_ASSET]: (node, children) =>
+            <Image
+                src={'https:' + node.data.target.fields.file.url}
+                alt={node.data.target.fields.description}
+                height={node.data.target.fields.file.details.image.height}
+                width={node.data.target.fields.file.details.image.width}
+            />
     }
 }
