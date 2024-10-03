60db13b33aa0870f576de34321a670d9f1a2fcc6
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-03 15:31:25 +1000

    init sidebar layout for a post page

--- app/posts/[slug]/layout.jsx
+++ app/posts/[slug]/layout.jsx
@@ -1,14 +1,45 @@
 import style from '@/styles/post.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
+import { getFeaturedPosts } from '@/config/contentful/client'
 
-export default function PostLayout({ children }) {
+export default async function PostLayout({ children }) {
+    const featuredPosts = await getFeaturedPosts();
     return (
         <div className={style.postLayout}>
             <aside>
-                <h2>Post Title</h2>
-                <p>Post Subtitle</p>
-                <p>Post Date</p>    
+                <section className={style.featuredPosts}>
+                    <hr className={style.divider} data-content="Featured Posts" />
+                    <ul style={{ listStyleType: 'none', display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                        {featuredPosts.items.map((post) =>
+                            <Link href={`/posts/${post.fields.postSlug}`}>
+                                <li key={post.sys.id} style={{ height: '100px', width: '100%', display: 'flex', gap: '10px', border: '1px solid var(--dark-blue)',borderRadius:'10px',padding:'10px' }}>
+
+                                    <div className={style.featuredPostImage} style={{ position: 'relative', height: '100%', width: '40%' }}>
+                                        <Image
+                                            src={'https:' + post.fields.mainImage.fields.file.url}
+                                            alt={post.fields.mainImage.fields.description}
+                                            fill={true}
+                                            style={{
+                                                width: '100%',
+                                                height: '100%',
+                                                objectFit: 'cover'
+                                            }}
+                                        />
+
+                                    </div>
+                                    <p style={{ width: '60%' }}> {post.fields.postTitle}</p>
+
+
+
+
+
+                                </li>
+                            </Link>)}
+                    </ul>
+                </section>
             </aside>
             {children}
-        </div>
+        </div >
     )
 }
