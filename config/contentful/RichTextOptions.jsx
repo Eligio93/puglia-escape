4fc2f94d908bf2973f2d6fad656fc549fcd26c52
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-03 15:29:29 +1000

    adds Hyperlink option and improves other options return

--- config/contentful/RichTextOptions.jsx
+++ config/contentful/RichTextOptions.jsx
@@ -1,17 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
+import style from '@/styles/post.module.css'
 
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 export const options = {
     renderNode: {
-        [INLINES.ENTRY_HYPERLINK]: (node, children) => <Link href={'/posts/' + node.data.target.fields.postSlug}>{node.content[0].value}</Link>,
-        [BLOCKS.EMBEDDED_ASSET]: (node, children) =>
-            <Image
-                src={'https:' + node.data.target.fields.file.url}
-                alt={node.data.target.fields.description}
-                height={node.data.target.fields.file.details.image.height}
-                width={node.data.target.fields.file.details.image.width}
-            />
+        //external link
+        [INLINES.HYPERLINK]: (node, children) => <a href={node.data.uri} target="_blank">{children}</a>,
+        //adds an inline hyperlink
+        [INLINES.ENTRY_HYPERLINK]: (node, children) => <Link href={'/posts/' + node.data.target.fields.postSlug}>{children}</Link>
+        ,
+        //adds a body image
+        [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+            if (node.data.target.fields.file.contentType.includes('image')) {
+                return (
+                    <div className={style.embeddedImage} style={{width: '70%',height: '400px', margin: '0 auto' }}>
+                        <Image
+                            src={'https:' + node.data.target.fields.file.url}
+                            alt={node.data.target.fields.description}
+                            height={node.data.target.fields.file.details.image.height}
+                            width={node.data.target.fields.file.details.image.width}
+                            style={{ width: '100%', height: '100%',objectFit:'cover' }}
+                        />
+                    </div>
+                )
+            }
+
+
+        }
+        //need to add if the contentype contains 'image'
+
     }
 }
