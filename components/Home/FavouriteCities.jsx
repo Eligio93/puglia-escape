f69c4522e2a5cedebdcd49f444360e09eb9b4cc4
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-04 10:23:16 +1000

    fix links with capital letters

--- components/Home/FavouriteCities.jsx
+++ components/Home/FavouriteCities.jsx
@@ -4,7 +4,7 @@ import lecceImg from '@/public/lecce.jpg'
 import monopoliImg from '@/public/monopoli.jpg'
 import polignanoImg from '@/public/polignanoAMare.jpg'
 import gallipoliImg from '@/public/gallipoli.jpg'
-import ostuniImg from   '@/public/ostuni.jpg'
+import ostuniImg from '@/public/ostuni.jpg'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -13,42 +13,42 @@ export default function FavouriteCities() {
         <section className={styles.favouriteCities}>
             <h2>Our Favourite Cities</h2>
             <div className={styles.citiesGrid}>
-                <Link href='/blog?city=lecce' className={styles.gridItemLecce}>
+                <Link href='/blog?city=Lecce' className={styles.gridItemLecce}>
                     <Image
                         src={lecceImg}
                         alt='Duomo of Lecce'
                     />
                     <p>Lecce</p>
                 </Link>
-                <Link href='/blog?city=monopoli' className={styles.gridItemMonopoli}>
+                <Link href='/blog?city=Monopoli' className={styles.gridItemMonopoli}>
                     <Image
                         src={monopoliImg}
                         alt='Bay of Monopoli City'
                     />
                     <p>Monopoli</p>
                 </Link>
-                <Link href='/blog?city=polignano' className={styles.gridItemPolignano}>
+                <Link href='/blog?city=Polignano A Mare' className={styles.gridItemPolignano}>
                     <Image
                         src={polignanoImg}
                         alt='High picture of Polignano a Mare'
                     />
                     <p>Polignano A Mare</p>
                 </Link>
-                <Link href='/blog?city=gallipoli' className={styles.gridItemGallipoli}>
+                <Link href='/blog?city=Gallipoli' className={styles.gridItemGallipoli}>
                     <Image
                         src={gallipoliImg}
                         alt='Harbour of Gallipoli'
                     />
                     <p>Gallipoli</p>
                 </Link>
-                <Link href='/blog?city=bari' className={styles.gridItemBari}>
+                <Link href='/blog?city=Bari' className={styles.gridItemBari}>
                     <Image
                         src={bariImg}
                         alt='Bari Theatre'
                     />
                     <p>Bari</p>
                 </Link>
-                <Link href='/blog?city=ostuni' className={styles.gridItemOstuni}>
+                <Link href='/blog?city=Ostuni' className={styles.gridItemOstuni}>
                     <Image
                         src={ostuniImg}
                         alt='High Picture of Ostuni'
