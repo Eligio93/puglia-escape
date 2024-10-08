68cc52944aa17ae77c3ca402e7db132da8ad68d4
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-08 18:10:18 +1100

    init component

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import style from '@/styles/footer.module.css'
+
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+            <p>Copyright 2022 Puglia Project</p>
+        </footer>
+    )
+}
