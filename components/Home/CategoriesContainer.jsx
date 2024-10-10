41e904196da650a80a1d41b4bdaa76f042dba934
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-10 13:08:46 +1100

    set Categories images

--- components/Home/CategoriesContainer.jsx
+++ components/Home/CategoriesContainer.jsx
@@ -1,36 +1,36 @@
 
 import Image from 'next/image'
 import categoryEatDrinkImage from '@/public/categoryEatDrink.jpg'
+import categoryExperienceImage from '@/public/categoryExperience.jpg'
 import categoryDiscoverImage from '@/public/categoryDiscover.jpg'
+import categoryEventsImage from '@/public/categoryEvents.jpg'
 import styles from '@/styles/home.module.css'
 import Link from 'next/link'
 export default function CategoriesContainer() {
     return (
         <section className={styles.categoriesContainer}>
-            <h2>Journey through Puglia</h2>
-            <p>Explore our insights</p>
-
+            <h2>Unveiling the Heart of Puglia</h2>
+            <p>Discover hidden gems, immerse yourself in unique experiences, and explore all that Puglia has to offer. Navigate through authentic journeys and unforgettable moments that bring the essence of this extraordinary region to life</p>
+            <hr />
             <div className={styles.categories}>
-
                 <div className={styles.category}>
                     <Image
                         src={categoryEatDrinkImage}
-                        alt='Two guys walking in the streets of Puglia'
-                        sizes="25vw"
+                        alt='A dinner table on the beach'
                     />
-                    <Link href={'/blog/categories/eat_drink'} className={styles.categoryCta}>
-                        Eat/Drink
+                    <Link href={'/blog?category=Eat+And+Drink'} className={styles.categoryCta}>
+                        Eat and Drink
                     </Link>
+                    <div className={styles.cover}></div>
                 </div>
 
                 <div className={styles.category}>
                     <Image
-                        src={categoryDiscoverImage}
-                        alt='Two guys walking in the streets of Puglia'
-                        sizes="25vw"
+                        src={categoryExperienceImage}
+                        alt='A person heading to the beach'
                     />
-                    <Link href={'/blog/categories/travel'} className={styles.categoryCta}>
-                        Travel
+                    <Link href={'/blog?category=Experience'} className={styles.categoryCta}>
+                        Experience
                     </Link>
 
                 </div>
@@ -39,23 +39,23 @@ export default function CategoriesContainer() {
                 <div className={styles.category}>
                     <Image
                         src={categoryDiscoverImage}
-                        alt='Two guys walking in the streets of Puglia'
-                        sizes="25vw"
+                        alt='Trullo at night with stars'
                     />
-                    <Link href={'/blog/categories/discover'} className={styles.categoryCta}>
+                    <Link href={'/blog?category=Discover'} className={styles.categoryCta}>
                         Discover
                     </Link>
 
                 </div>
                 <div className={styles.category}>
                     <Image
-                        src={categoryDiscoverImage}
-                        alt='Two guys walking in the streets of Puglia'
-                        sizes="25vw"
+                        src={categoryEventsImage}
+                        alt='Picture of the Polifonic Event in Puglia'
                     />
-                    <Link href={'/blog/categories/events'} className={styles.categoryCta}>
+                    <Link href={'/blog?category=Events'} className={styles.categoryCta}>
                         Events
                     </Link>
+
+
                 </div>
             </div>
         </section >
