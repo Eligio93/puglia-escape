ae26d90163ffea7826820210f3d95294c0cbaaf5
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-11 13:24:28 +1100

    call the right function in case there s a search value query

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,4 +1,4 @@
-import { getPosts } from "@/config/contentful/client";
+import { getPosts, getPostsBySearch } from "@/config/contentful/client";
 import BlogPosts from "@/components/Blog/BlogPosts";
 import styles from '@/styles/blog.module.css'
 
@@ -7,7 +7,15 @@ import styles from '@/styles/blog.module.css'
 export default async function Blog({ searchParams }) {
     const cityQuery = searchParams.city ? 'city' + searchParams.city.split(' ').join('') : null;
     const categoryQuery = searchParams.category ? 'category' + searchParams.category.split(' ').join('') : null;
-    const posts = await getPosts(categoryQuery, cityQuery);
+    const searchQuery = searchParams.searchValue
+    let posts;
+    if (searchQuery) {
+        posts = await getPostsBySearch(searchQuery);
+    } else {
+        posts = await getPosts(categoryQuery, cityQuery);
+    }
+
+
 
 
     //controllare i parametri e fare il get post con i parametri selezionati altrimenti gettutti i post
