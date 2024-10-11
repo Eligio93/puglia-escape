8cdf4f3654b964ac5a947a93b299f01d7b09e6fa
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-11 13:24:00 +1100

    adds function to redirect to the search results page

--- components/Header/Searchbar.jsx
+++ components/Header/Searchbar.jsx
@@ -2,15 +2,27 @@
 import Image from 'next/image'
 import searchIcon from '@/public/searchIcon.svg'
 import styles from '@/styles/searchbar.module.css'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 
 export default function SearchBar() {
+    const [searchValue, setSearchValue] = useState('')
+    const router = useRouter()
+
+    function handleSearch(e) {
+        e.preventDefault();
+        if (searchValue.trim() !== '') {
+            router.push(`/blog?searchValue=${searchValue}`)
+        }
+    }
+
     return (
         <form className={styles.searchbar}>
             <label htmlFor="searchBar">
-            <input type="text" name='searchValue' id='searchBar'/>
+                <input type="text" name='searchValue' id='searchBar' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
             </label>
-            <button className={styles.searchBtn} type='submit' onClick={() => alert('ciao')}>
+            <button className={styles.searchBtn} type='submit' onClick={handleSearch}>
                 <Image
                     src={searchIcon}
                     alt='searchbar icon'
