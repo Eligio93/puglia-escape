12f7ecd6e5a93928754817273bfcb187a05d0cee
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-14 22:24:02 +1100

    first structure of the component

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,10 +1,47 @@
+import projectPugliaLogo from '@/public/pugliaProjectLogo.webp'
+
+
+
+
 import style from '@/styles/footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 export default function Footer() {
     return (
         <footer className={style.footer}>
-            <p>Copyright 2022 Puglia Project</p>
+
+            <div className={style.footerInfoContainer}>
+                <div className={style.footerLogoContainer}>
+                    <Image
+                        src={projectPugliaLogo}
+                        alt="logo puglia project"
+                    />
+                </div>
+                <div className={style.footerInfo}>
+                    <ul>
+                        <p>Blog</p>
+                        <Link href='/blog?category=Eat+And+Drink'><li>Eat And Drink</li></Link> 
+                        <Link href='/blog?category=Experience'><li>Experience</li></Link>
+                        <Link href='/blog?category=Events'><li>Events</li></Link>
+                        <Link href='/blog?category=Discover'><li>Discover</li></Link>
+                    </ul>
+                    <ul>
+                        <p>Puglia Project</p>
+                        <Link href='/about'><li>About</li></Link>
+                        <a href="mailto:info@pugliaproject.com">info@pugliaproject.com</a>
+                    </ul>
+
+                </div>
+            </div>
+
+            <div className={style.footerCopyright}>
+
+                <p>© 2023 Puglia Project. All rights reserved. Developed by <a href='https://ec-webdev.netlify.app/' target='_blank'>Eligio Cristantielli</a></p>
+            </div>
+
         </footer>
+
     )
 }
