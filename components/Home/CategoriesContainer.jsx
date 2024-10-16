6aefa8d57e0784abe820feac25fb96152ba1d849
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-16 13:04:16 +1100

    fix bug on Link and format document

--- components/Home/CategoriesContainer.jsx
+++ components/Home/CategoriesContainer.jsx
@@ -13,50 +13,51 @@ export default function CategoriesContainer() {
             <p>Discover hidden gems, immerse yourself in unique experiences, and explore all that Puglia has to offer. Navigate through authentic journeys and unforgettable moments that bring the essence of this extraordinary region to life</p>
             <hr />
             <div className={styles.categories}>
-                <div className={styles.category}>
+
+                <Link href={'/blog?category=Eat+And+Drink'} className={styles.category}>
                     <Image
                         src={categoryEatDrinkImage}
                         alt='A dinner table on the beach'
                     />
-                    <Link href={'/blog?category=Eat+And+Drink'} className={styles.categoryCta}>
+                    <div className={styles.categoryCta}>
                         Eat and Drink
-                    </Link>
-                    <div className={styles.cover}></div>
-                </div>
+                    </div>
+                    {/* <div className={styles.cover}></div> */}
+                </Link>
 
-                <div className={styles.category}>
+                <Link href={'/blog?category=Experience'} className={styles.category}>
                     <Image
                         src={categoryExperienceImage}
                         alt='A person heading to the beach'
                     />
-                    <Link href={'/blog?category=Experience'} className={styles.categoryCta}>
+                    <div className={styles.categoryCta}>
                         Experience
-                    </Link>
+                    </div>
 
-                </div>
+                </Link>
 
 
-                <div className={styles.category}>
+                <Link href={'/blog?category=Discover'} className={styles.category}>
                     <Image
                         src={categoryDiscoverImage}
                         alt='Trullo at night with stars'
                     />
-                    <Link href={'/blog?category=Discover'} className={styles.categoryCta}>
+                    <div className={styles.categoryCta}>
                         Discover
-                    </Link>
+                    </div>
 
-                </div>
-                <div className={styles.category}>
+                </Link>
+                <Link href={'/blog?category=Events'} className={styles.category}>
                     <Image
                         src={categoryEventsImage}
                         alt='Picture of the Polifonic Event in Puglia'
                     />
-                    <Link href={'/blog?category=Events'} className={styles.categoryCta}>
+                    <div className={styles.categoryCta}>
                         Events
-                    </Link>
+                    </div>
 
 
-                </div>
+                </Link>
             </div>
         </section >
     )
