cd4619f09364477cb734577a343fffe19fbb2889
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-18 15:21:37 +1100

    adds heading visible only in mobile

--- components/Post/SharingSidebar.jsx
+++ components/Post/SharingSidebar.jsx
@@ -4,8 +4,10 @@ import styles from '@/styles/post.module.css'
 export default function SharingSidebar() {
     return (
         <aside className={styles.sharingSidebar}>
+            <hr />
+            <h3>Share On:</h3>
             <ShareButtons />
-         
+
         </aside>
     )
 }
