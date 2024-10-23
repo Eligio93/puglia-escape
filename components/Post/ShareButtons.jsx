f12ef46a58091d13a3b3dba28cc6ce52b033b784
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-23 15:37:57 +1100

    make the component as client as involve interaction

--- components/Post/ShareButtons.jsx
+++ components/Post/ShareButtons.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import copyUrlIcon from '@/public/copyUrlIcon.svg'
 import facebookIcon from '@/public/facebookIconB.svg'
 import whatsappIcon from '@/public/whatsappIcon.svg'
@@ -5,6 +7,7 @@ import linkedinIcon from '@/public/linkedinIcon.svg'
 import xIcon from '@/public/xIcon.svg'
 import Image from 'next/image'
 import style from '@/styles/post.module.css'
+import { usePathname } from 'next/navigation'
 
 //TWITTER: https://twitter.com/intent/tweet
 //FACEBOOK: https://www.facebook.com/sharer/sharer.php
@@ -12,18 +15,20 @@ import style from '@/styles/post.module.css'
 //LINKEDIN: https://www.linkedin.com/shareArticle
 
 export default function ShareButtons() {
+    const path = 'http://localhost:3000' + usePathname();
     return (
         <div className={style.shareButtons}>
             <Image
                 src={copyUrlIcon}
                 alt='copy url icon'
                 title='Copy URL' />
-
+            <a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURI(path)}`}>
             <Image
                 src={facebookIcon}
                 alt='facebook icon'
                 title='Share on Facebook'
             />
+            </a>
             <Image
                 src={whatsappIcon}
                 alt='whatsapp icon'
