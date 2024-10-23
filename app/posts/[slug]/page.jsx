736a4534bed84d84be6e00bb6127c09ebaa08184
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-23 15:41:30 +1100

    imports sharing Sidebar for mobile menu

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -4,10 +4,9 @@ import PostHeader from "@/components/Post/PostHeader";
 import PostHero from "@/components/Post/PostHero";
 import PostBody from "@/components/Post/PostBody";
 import styles from '@/styles/post.module.css'
-
+import SharingSidebar from "@/components/Post/SharingSidebar";
 import Link from "next/link";
 import Image from "next/image";
-import { min } from "date-fns";
 
 export async function generateMetadata({ params }) {
     const slug = params.slug;
@@ -23,9 +22,9 @@ export async function generateMetadata({ params }) {
             //page description
             description: post.items[0].fields.seo.fields.seoDescription,
             //url del sito 'https://pugliaProject.com
-            url: post.items[0].fields.seo.fields.ogurl,
+            url: 'https://localhost:3000',
             //Name of the site
-            siteName: 'Puglia Project',
+            siteName: 'From Puglia',
             //type is article
             type: 'article',
             // image must m
@@ -43,7 +42,6 @@ export default async function Post({ params }) {
     const slug = params.slug;
     const post = await getPostBySlug(slug)
     const relatedPosts = post.items[0].fields.relatedPosts
-    console.log(relatedPosts[0].fields.mainImage.fields.file)
     return (
         <>
             <div className={styles.postContainer}>
@@ -54,7 +52,9 @@ export default async function Post({ params }) {
                     <PostBody post={post} />
                 </article>
             </div>
-            <section className={styles.relatedPostsContainer} style={{ backgroundColor: 'var(--blue)', color: 'white',padding:'30px' }}>
+            <SharingSidebar />
+
+            <section className={styles.relatedPostsContainer} >
                 <h2>Related Posts</h2>
                 <hr />
                 <ul>
@@ -70,7 +70,7 @@ export default async function Post({ params }) {
                                             width={post.fields.mainImage.fields.file.details.image.width}
                                         />
                                     </div>
-                                    <p>{post.fields.postTitle}</p>
+                                    <p className={styles.relatedPostTitle}>Polignano a Mare: Where the Sea Meets Magic</p>
 
                                 </li>
                             </Link>
