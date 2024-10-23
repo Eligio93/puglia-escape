9036bdf385969b6b47fc2e8c5a37decf6229db00
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-23 15:45:53 +1100

    init component with metadata

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import Loading from "../loading"
+
+export const metadata = {
+    title: "About - From Puglia",
+    description: "Learn more about From Puglia, created by a local, passionate about sharing the best of Puglia. Discover our mission to bring this region from Puglia to the World.",
+};
+
+export default async function About() {
+    return (
+        <Loading />
+    )
+}
