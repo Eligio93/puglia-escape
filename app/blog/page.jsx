99a815a6013f3d6193ec270617430d97a2fd2e81
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-23 15:42:02 +1100

    adds page title and description

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,6 +2,11 @@ import { getPosts, getPostsBySearch } from "@/config/contentful/client";
 import BlogPosts from "@/components/Blog/BlogPosts";
 import styles from '@/styles/blog.module.css'
 
+export const metadata = {
+    title: "Blog - From Puglia",
+    description: "Welcome to the From Puglia blog, your go-to source for discovering Puglia. Find articles on must-visit spots, local traditions, and insider travel experiences from the heart of Italy’s heel.",
+};
+
 
 
 export default async function Blog({ searchParams }) {
@@ -9,16 +14,14 @@ export default async function Blog({ searchParams }) {
     const categoryQuery = searchParams.category ? 'category' + searchParams.category.split(' ').join('') : null;
     const searchQuery = searchParams.searchValue
     let posts;
+
+    //Gets posts based on query params  
     if (searchQuery) {
         posts = await getPostsBySearch(searchQuery);
     } else {
         posts = await getPosts(categoryQuery, cityQuery);
     }
 
-
-
-
-    //controllare i parametri e fare il get post con i parametri selezionati altrimenti gettutti i post
     return (
         <div className={styles.blog}>
             <BlogPosts
