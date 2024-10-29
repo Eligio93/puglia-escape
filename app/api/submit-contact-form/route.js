d351faf2143d14517ca19d4a6cff0c549ccc0bb0
Eligio93 <eligio.cristantielli@gmail.com>
2024-10-29 13:02:01 +1100

    sanitizes data coming from contact form

--- app/api/submit-contact-form/route.js
+++ app/api/submit-contact-form/route.js
@@ -1,11 +1,18 @@
 import ContactFormSubmission from "@/components/Mail_Templates/ContactFormSubmission";
 import { Resend } from "resend";
+import validator from "validator";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
     const requestBody = await request.json();
     const { name, lastName, email, message } = requestBody;
+    
+    //Sanitize Data
+    name = validator.escape(name.trim())
+    lastName = validator.escape(lastName.trim());
+    email = validator.normalizeEmail(email.trim());
+    message = validator.escape(message.trim());
     try {
         const { data, error } = await resend.emails.send({
             from: 'From Puglia Contact Form <hello@frompuglia.com>',
