9658db2aa373cc31353e83db75c222f0257c7f02
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-14 02:01:53 +1100

    set the sitemap to be rendered dynamically on vercel

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,26 +1,28 @@
-import { getPosts } from "@/config/contentful/client"
+import { getPosts } from "@/config/contentful/client";
 
-export default async function sitemap() {
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const dynamicParams = true;
+
+export async function sitemap() {
   const posts = (await getPosts()).items;
-  const postsEntries = posts.map(post => (
-    {
-      url: `${process.env.NEXT_PUBLIC_SITE_URL}/posts/${post.fields.postSlug}`,
-      lastModified: new Date(post.sys.updatedAt)
-    }
-  ))
+  const postsEntries = posts.map((post) => ({
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/posts/${post.fields.postSlug}`,
+    lastModified: new Date(post.sys.updatedAt),
+  }));
   return [
     {
-      url: 'https://frompuglia.com',
-      changeFrequency: 'yearly',
+      url: "https://frompuglia.com",
+      changeFrequency: "yearly",
     },
     {
-      url: 'https://frompuglia.com/blog',
-      changeFrequency: 'monthly',
+      url: "https://frompuglia.com/blog",
+      changeFrequency: "monthly",
     },
     {
-      url: 'https://frompuglia.com/about',
-      changeFrequency: 'yearly',
+      url: "https://frompuglia.com/about",
+      changeFrequency: "yearly",
     },
-    ...postsEntries
-  ]
+    ...postsEntries,
+  ];
 }
