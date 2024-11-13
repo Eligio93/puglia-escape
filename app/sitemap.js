3caf45b3953d8cf352a73ed13e4ee87ec721b9c8
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-14 02:03:09 +1100

    fix bug on export

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const dynamicParams = true;
 
-export async function sitemap() {
+export default async function sitemap() {
   const posts = (await getPosts()).items;
   const postsEntries = posts.map((post) => ({
     url: `${process.env.NEXT_PUBLIC_SITE_URL}/posts/${post.fields.postSlug}`,
